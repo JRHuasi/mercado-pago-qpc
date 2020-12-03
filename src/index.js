7b0381b07aec2ae7748602fc6894494ae6f8680b
jrhuasi <julianra@gmail.com>
2020-12-03 14:16:15 -0300

    paga por MercadoPago

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const mercadopago = require ('mercadopago');
+
+// middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// Agrega credenciales
+mercadopago.configure({
+  access_token: 'APP_USR-6623451607855904-111502-1f258ab308efb0fb26345a2912a3cfa5-672708410'
+});
+
+// routes
+app.post('/checkout',(req, res) => {
+  
+  let preference = {
+    items: [
+      {
+        title: req.body.title,
+        unit_price: parseInt(req.body.price),
+        quantity: 1,
+      }
+    ]
+  };
+
+  mercadopago.preferences.create(preference)
+    .then(function(response){
+      // console.log(response.body);
+      res.redirect(response.body.init_point);
+    }).catch(function(error){
+      console.log(error);
+    });
+
+
+})
+
+//server
+app.listen(3000, ()=>console.log("Server on port 3000"));
+
